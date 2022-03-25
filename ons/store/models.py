bcc216f088b90d995804a70f10b9dd7b4b6e556c
RushikeshKarwa <karwarb19.comp@coep.ac.in>
2022-03-25 11:28:02 +0530

    cretated product and category model next tsk to render them to home page

--- ons/store/models.py
+++ ons/store/models.py
@@ -1,3 +1,44 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name=models.CharField(max_length=250, unique=True)
+    slug=models.SlugField(max_length=250, unique=True)
+    description=models.TextField(blank=True)
+    image=models.ImageField(upload_to='category', blank=True)
+
+    #to set correct plural for the model changing the name in the db 
+    class Meta:
+        ordering=('name',)
+        verbose_name='category'
+        verbose_name_plural='categories'
+
+    #to create human readable formatting
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name=models.CharField(max_length=250, unique=True)
+    slug=models.SlugField(max_length=250, unique=True)
+    description=models.TextField(blank=True)
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+    image=models.ImageField(upload_to='product', blank=True)
+    stock=models.IntegerField()
+    available=models.BooleanField(default=True)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+    #to set correct plural for the model changing the name in the db 
+    class Meta:
+        ordering=('name',)
+        verbose_name='product'
+        verbose_name_plural='products'
+
+    #to create human readable formatting
+
+    #to dispaly product name
+    def __str__(self):
+        return self.name
