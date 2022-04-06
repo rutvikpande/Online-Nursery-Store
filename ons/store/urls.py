6da20901c3e83cfb1702f0759592853fdbd8ae23
RushikeshKarwa <karwarb19.comp@coep.ac.in>
2022-04-06 21:51:13 +0530

    added search featuregit status

--- ons/store/urls.py
+++ ons/store/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('account/signout/', views.signoutView, name='signout'),
     path('order_history/', views.orderHistory, name='order_history'),
     path('order/<int:order_id>', views.viewOrder, name='order_detail'),
+    path('search/', views.search, name='search'),
 ]
