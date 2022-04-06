72c67958b66078e496c4ddb3a45f301f9fae55de
Rutvik Pande <panderutvik123@gmail.com>
2022-04-06 16:57:00 +0530

    Designed login,signup and user authentication service and handled database part

--- ons/ons/settings.py
+++ ons/ons/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'store',
     'stripe',
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -128,6 +130,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR, 'static' , 'media')
 STRIPE_PUBLISHABLE_KEY = 'pk_test_51KlSPmKO7WZhffnQSauxlMotUkKc2i3PP7P5JXmQRIh3sIolvtSYbcgoLjthKr6DhgFN6G0BRljLWAOXSVW9fEyR00tjMeAz9c'
 STRIPE_SECRET_KEY = 'sk_test_51KlSPmKO7WZhffnQpM5nVmR0CShVCPTSIh05pfETnwDaP2JLnx9Jb4aWm8FAI9Abc6q9flhzsjZas1zo9GIyto0u00cf4EuFic'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- ons/store/views.py
+++ ons/store/views.py
@@ -3,6 +3,10 @@ from .models import Category,Product,Cart,CartItem, Order, OrderItem
 from django.core.exceptions import ObjectDoesNotExist
 import stripe
 from django.conf import settings
+from django.contrib.auth.models import Group, User
+from .forms import SignUpForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
 
 def home(request, category_slug=None):
     category_page=None
@@ -165,3 +169,37 @@ def thanks_page(request, order_id):
     if order_id:
         customer_order = get_object_or_404(Order, id=order_id)
     return render(request, 'thankyou.html', {'customer_order': customer_order})
+
+def signupView(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            signup_user = User.objects.get(username=username)
+            customer_group = Group.objects.get(name='Customer')
+            customer_group.user_set.add(signup_user)
+            login(request, signup_user)
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def signinView(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return redirect('signup')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'signin.html', {'form': form})
+
+def signoutView(request):
+    logout(request)
+    return redirect('signin')
