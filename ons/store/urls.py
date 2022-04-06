ce471791f2ab5bfd027af19c398bba1a351f0baa
RushikeshKarwa <karwarb19.comp@coep.ac.in>
2022-04-06 21:35:01 +0530

    completed purchase history feature and user now can print Order Detail

--- ons/store/urls.py
+++ ons/store/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('account/create/', views.signupView, name='signup'),
     path('account/signin/', views.signinView, name='signin'),
     path('account/signout/', views.signoutView, name='signout'),
+    path('order_history/', views.orderHistory, name='order_history'),
+    path('order/<int:order_id>', views.viewOrder, name='order_detail'),
 ]
